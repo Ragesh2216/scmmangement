c89475acf9ac950684414e383c7f83dbd269d86e
Ragesh2216 <rageshwaran@thestackly.com>
2025-11-26 10:26:53 +0530

    Update image imports in About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import sakthi from '../images/sakthi.webp';
+import sakthi from '../images/www.webp';
 import abi from '../images/abi.webp';
 import dhara from '../images/soe.webp';
-import yuva from '../images/www.webp';
+import yuva from '../images/yuva.webp';
 import { Link, useNavigate } from "react-router-dom";
 
 const About = () => {
